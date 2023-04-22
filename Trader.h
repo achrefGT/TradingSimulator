03de39ad0e553b0f632ade64e7bdbe85a188b604
achrefGT <130295436+achrefGT@users.noreply.github.com>
2023-04-22 22:01:57 +0100

    Update Trader.h

--- Trader.h
+++ Trader.h
@@ -3,6 +3,7 @@
 #include "Transaction.h"
 #include "Portefeuille.h"
 #include "Date.h"
+#include <random>
 
 using namespace std;
 
@@ -18,11 +19,37 @@ class TraderAleatoire : public Trader {
 
 } ;
 
-Transaction TraderAleatoire::choisirTransaction(const Bourse& bourse, const Portefeuille &portefeuille){
-    vector<PrixJournalier> prixJournalierDisponibles = bourse.getPrixJournaliersParDate(bourse.getDateFinRecherche());
-
-
+Transaction TraderAleatoire::choisirTransaction(const Bourse& bourse, const Portefeuille& portefeuille) {
+    vector<PrixJournalier> actionsDisponibles = bourse.getPrixJournaliersParDate(bourse.getDateFinRecherche(), portefeuille.getSolde());
+    int choix = rand() % 3;
+    if (actionsDisponibles.empty()) {
+        choix = rand() % 2;
+    }
+    if (portefeuille.getTitres().empty()) {
+        choix = 2;
+    }
+    typeTransaction type = rien;
+    int i = 0, taille = 0, quantite = 0;
+    switch (choix) {
+        case 0:
+            type = rien;
+            break;
+        case 1:
+            type = vente;
+            taille = portefeuille.getTitres().size();
+            i = rand() % taille;
+            quantite = 1 + rand() % (portefeuille.getTitres()[i].getQuantite());
+            break;
+        case 2:
+            type = achat;
+            taille = actionsDisponibles.size();
+            i = rand() % taille;
+            do {
+                quantite = 1 + rand() % 100;
+            } while (quantite * actionsDisponibles[i].getPrix() > portefeuille.getSolde());
+            break;
+    }
+    return Transaction(actionsDisponibles[i].getNomAction(), quantite, type);
 }
-
 
 #endif // TRADER_H_INCLUDED

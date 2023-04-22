080c9cfb2f882fd4b6dcae2d50f84eb22351eacd
achrefGT <130295436+achrefGT@users.noreply.github.com>
2023-04-22 22:08:05 +0100

    Update Trader.h

--- Trader.h
+++ Trader.h
@@ -20,13 +20,13 @@ class TraderAleatoire : public Trader {
 } ;
 
 Transaction TraderAleatoire::choisirTransaction(const Bourse& bourse, const Portefeuille& portefeuille) {
-    vector<PrixJournalier> actionsDisponibles = bourse.getPrixJournaliersParDate(bourse.getDateFinRecherche(), portefeuille.getSolde());
+    vector<PrixJournalier> actionsDisponibles = bourse.getPrixJournaliersParDate(bourse.getDateFinRecherche(), portefeuille.getSolde()); // avoir une copie des prix journaliers disponibles
     int choix = rand() % 3;
     if (actionsDisponibles.empty()) {
-        choix = rand() % 2;
+        choix = rand() % 2;   // si il n'y a pas des action dans la bourse soit on vend des titres ou on fait rien
     }
     if (portefeuille.getTitres().empty()) {
-        choix = 2;
+        choix = 2;           // si le portefeuille est vide on achete
     }
     typeTransaction type = rien;
     int i = 0, taille = 0, quantite = 0;
@@ -38,7 +38,7 @@ Transaction TraderAleatoire::choisirTransaction(const Bourse& bourse, const Port
             type = vente;
             taille = portefeuille.getTitres().size();
             i = rand() % taille;
-            quantite = 1 + rand() % (portefeuille.getTitres()[i].getQuantite());
+            quantite = 1 + rand() % (portefeuille.getTitres()[i].getQuantite()); // la quantite a vendre ne doit pas dépasser ce qu'il y a dans la portefeuille
             break;
         case 2:
             type = achat;
@@ -46,7 +46,7 @@ Transaction TraderAleatoire::choisirTransaction(const Bourse& bourse, const Port
             i = rand() % taille;
             do {
                 quantite = 1 + rand() % 100;
-            } while (quantite * actionsDisponibles[i].getPrix() > portefeuille.getSolde());
+            } while (quantite * actionsDisponibles[i].getPrix() > portefeuille.getSolde()); // il faut pas depasser le solde dans le portefeuille
             break;
     }
     return Transaction(actionsDisponibles[i].getNomAction(), quantite, type);
